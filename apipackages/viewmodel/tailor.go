d25a52987252f3334b89c49bdf03bd4ca45a1732
edberto <edbertongko88@gmail.com>
2020-08-16 09:30:04 +0700

    add json tag

--- apipackages/viewmodel/tailor.go
+++ apipackages/viewmodel/tailor.go
@@ -2,17 +2,17 @@ package viewmodel
 
 type (
 	TailorVM struct {
-		ID            int
-		ModelID       int
-		MaterialID    int
-		ModelName     string
-		MaterialName  string
-		MaterialColor string
-		UUID          string
-		Name          string
-		Phone         string
-		Email         string
-		Address       string
-		Price         float64
+		ID            int     `json:"id"`
+		ModelID       int     `json:"model_id"`
+		MaterialID    int     `json:"materila_id"`
+		ModelName     string  `json:"model_name"`
+		MaterialName  string  `json:"material_name"`
+		MaterialColor string  `json:"material_color"`
+		UUID          string  `json:"uuid"`
+		Name          string  `json:"name"`
+		Phone         string  `json:"phone"`
+		Email         string  `json:"email"`
+		Address       string  `json:"address"`
+		Price         float64 `json:"price"`
 	}
 )
--- apipackages/viewmodel/user.go
+++ apipackages/viewmodel/user.go
@@ -2,14 +2,14 @@ package viewmodel
 
 type (
 	UserVM struct {
-		ID       int
-		TailorID int
-		Phone    string
-		Email    string
-		Name     string
-		Password string
-		Role     string
-		Username string
-		UUID     string
+		ID       int `json:"id"`
+		TailorID int `json:"tailor_id"`
+		Phone    string `json:"phone"`
+		Email    string `json:"email"`
+		Name     string `json:"name"`
+		Password string `json:"password"`
+		Role     string `json:"role"`
+		Username string `json:":"username"`
+		UUID     string `json:"uuid"`
 	}
 )
